81e041ca96aa253f03dd8667d5f33458b5f6b070
bharath <bharathp319@gmail.com>
2020-12-26 12:20:23 +0530

    added addition feature

---
+++ com/project/module/cal/Addition.py
@@ -0,0 +1,10 @@
+def even_odd():
+
+    even = []
+    odd = []
+    num = int(input("enter a number"))
+    if num % 2 == 0:
+        even.append(num)
+    else:
+       odd.append(num)
+    print(even, odd)
